5e38bf09800c20a0fdba1376844c21e1dfbfbec4
KimHyunSoo <goumuya@naver.com>
2025-04-29 17:57:49 +0900

    Clean upload: client/server 직접 push

---
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = 'eaf51a83dcaa7450f38cf8cb4f5e6183';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
